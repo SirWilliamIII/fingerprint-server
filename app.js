5508a38d00dfc4644eb511f3cbeb94a4cd6a28d8
SirWilliamIII <willcarpenterwastaken@gmail.com>
2024-07-05 16:50:46 -0500

    added full response

--- app.js
+++ app.js
@@ -17,34 +17,37 @@ app.use(express.static(path.join(__dirname, "public")));
 // Replace with your actual API key
 const IPSTACK_API_KEY = "2021aa2a34c2699161cd5d0967128bde";
 
-app.get("/", async(req, res) => {
-    try {
-        async function getClientIp(req) {
-            try {
-                const response = await axios.get("https://api.ipify.org?format=json");
-                return response.data.ip;
-            } catch (error) {
-                console.error("Error fetching client IP:", error);
-                return null;
-            }
-        }
-
-        const ip = await getClientIp(req);
-        if (!ip) {
-            throw new Error("Unable to fetch client IP");
-        }
-        console.log("Client IP:", ip);
-
-        const response = await axios.get(
-            `http://api.ipstack.com/${ip}?access_key=${IPSTACK_API_KEY}`
-        );
-        res.render("home", { ipData: response.data });
-    } catch (error) {
-        console.error("Error fetching IP data:", error);
-        res.render("home", { error: "Unable to fetch IP data" });
+app.get("/", async (req, res) => {
+  try {
+    async function getClientIp(req) {
+      try {
+        const response = await axios.get("https://api.ipify.org?format=json");
+        return response.data.ip;
+      } catch (error) {
+        console.error("Error fetching client IP:", error);
+        return null;
+      }
     }
+
+    const ip = await getClientIp(req);
+    if (!ip) {
+      throw new Error("Unable to fetch client IP");
+    }
+
+    for (let i = 0; i < Object.keys(ip).length; i++) {
+      console.log(Object.keys(ip)[i] + " : " + Object.values(ip)[i]);
+    }
+
+    const response = await axios.get(
+      `http://api.ipstack.com/${ip}?access_key=${IPSTACK_API_KEY}`
+    );
+    res.render("home", { ipData: response.data });
+  } catch (error) {
+    console.error("Error fetching IP data:", error);
+    res.render("home", { error: "Unable to fetch IP data" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
