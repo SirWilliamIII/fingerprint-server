3e5b55cc7d478ea04edb2ecb1c8b431041068d0a
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-07-06 15:30:41 -0500

    log browser data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const axios = require("axios");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,38 +20,52 @@ app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Replace with your actual API key
+app.use(bodyParser.json());
 
-app.get("/", async (req, res) => {
-  try {
-    async function getClientIp(req) {
-      try {
-        const response = await axios.get("https://api.ipify.org?format=json");
-        return response.data.ip;
-      } catch (error) {
-        console.error("Error fetching client IP:", error);
-        return null;
-      }
+app.get("/", async(req, res) => {
+    try {
+        async function getClientIp(req) {
+            try {
+                const response = await axios.get("https://api.ipify.org?format=json");
+                return response.data.ip;
+            } catch (error) {
+                console.error("Error fetching client IP:", error);
+                return null;
+            }
+        }
+
+        const ip = await getClientIp(req);
+        if (!ip) {
+            throw new Error("Unable to fetch client IP");
+        }
+
+        for (let i = 0; i < Object.keys(ip).length; i++) {
+            console.log(Object.keys(ip)[i] + " : " + Object.values(ip)[i]);
+        }
+
+        const response = await axios.get(
+            `http://api.ipstack.com/${ip}?access_key=2021aa2a34c2699161cd5d0967128bde`
+        );
+        res.render("home", { ipData: response.data });
+    } catch (error) {
+        console.error("Error fetching IP data:", error);
+        res.render("home", { error: "Unable to fetch IP data" });
     }
+});
 
-    const ip = await getClientIp(req);
-    if (!ip) {
-      throw new Error("Unable to fetch client IP");
-    }
+app.post("/log-browser-data", (req, res) => {
+    const browserData = req.body;
+    const clientIp =
+        req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-    for (let i = 0; i < Object.keys(ip).length; i++) {
-      console.log(Object.keys(ip)[i] + " : " + Object.values(ip)[i]);
-    }
+    console.log("Received browser data:", browserData);
+    console.log("Client IP address:", clientIp);
 
-    const response = await axios.get(
-      `http://api.ipstack.com/${ip}?access_key=2021aa2a34c2699161cd5d0967128bde`
-    );
-    res.render("home", { ipData: response.data });
-  } catch (error) {
-    console.error("Error fetching IP data:", error);
-    res.render("home", { error: "Unable to fetch IP data" });
-  }
+    // Here you can log the data to a file, database, etc.
+    // For demonstration, we'll just send a success response
+    res.json({ status: "success", data: browserData, ip: clientIp });
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
