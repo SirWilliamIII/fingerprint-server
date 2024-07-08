44fd5da5411b5ea362364b0ab8b155d205949357
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-07-07 22:43:03 -0500

    out of api tokens

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const getClientIp = require("./helpers/getClientIp"); // Adjust the path as necessary
-
+const getClientIp = require("./helpers/getClientIp");
 const app = express();
 
 const key = process.env.IPSTACK_API_KEY;
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 app.get("/", async (req, res) => {
   try {
     const ip = await getClientIp(req);
+
     if (!ip) {
       throw new Error("Unable to fetch client IP");
     }
@@ -31,6 +32,7 @@ app.get("/", async (req, res) => {
     const response = await axios.get(
       `http://api.ipstack.com/${ip}?access_key=${key}`
     );
+    console.log(response.data);
 
     res.render("home", {
       ipData: response.data,
--- helpers/fetchVisitorHistory.js
+++ helpers/fetchVisitorHistory.js
@@ -3,34 +3,23 @@ const {
   Region,
 } = require("@fingerprintjs/fingerprintjs-pro-server-api");
 
-const fprintKey = process.env.FINGERPRINT_API_KEY;
+async function fetchVisitorHistory(visitorId, req) {
+  const client = new FingerprintJsServerApiClient({
+    apiKey: process.env.FINGERPRINTJS_API_KEY,
+    region: Region.Global,
+  });
 
-const client = new FingerprintJsServerApiClient({
-  apiKey: fprintKey,
-  region: Region.Global,
-});
+  const vID = "2FgZMaeTJbVKRBYweA7H";
 
-const vID = "2FgZMaeTJbVKRBYweA7H";
-
-async function fetchVisitorHistory(visitorId) {
   const apiKey = client.apiKey; // Replace with your actual API key
-  const baseUrl = "https://api.fpjs.io"; // Base URL for the FingerprintJS Pro API
+  const baseUrl = "https://api.fpjs.io/"; // Base URL for the FingerprintJS Pro API
   const userAgent = req.headers["user-agent"];
   // Define your query parameters
-  const queryParams = {
-    visitorId: visitorId, // Assuming visitorId is the parameter you need to pass
-    limit: 5, // Example additional parameter
-    start: 0, // Example additional parameter
-    userAgent: userAgent, // Example additional parameter
-  };
 
   try {
     // Make the API call with query parameters
-    await client.fetch(`${baseUrl}/visitors`, {
-      params: queryParams,
-      headers: {
-        Authorization: `Bearer ${apiKey}`, // Assuming the API uses Bearer token authentication
-      },
+    await client.fetch(`${baseUrl}/visitors/${vID}?api_key=${client.apiKey}`, {
+      // Assuming the API uses Bearer token authentication
     });
 
     // Process the response
